fb7fcfd330801227aa20bf8ebc32fea8b0eb09ae
shangar21 <sharanshangar@gmail.com>
2024-02-17 11:36:24 -0500

    make utd

--- config/lua/keymaps.lua
+++ config/lua/keymaps.lua
@@ -2,6 +2,7 @@ local keymap = vim.keymap
 local api = vim.api
 local uv = vim.loop
 
+
 -- Set leader key
 vim.g.mapleader = ","
 vim.g.maplocalleader = vim.g.mapleader
@@ -17,6 +18,10 @@ keymap.set("n", "<Right>", "<C-W>l")
 keymap.set("n", "<Up>", "<C-W>k")
 keymap.set("n", "<Down>", "<C-W>j")
 
+-- Move between buffers
+keymap.set("n", "<S-l>", "<CMD>bnext<CR>")
+keymap.set("n", "<S-h>", "<CMD>bprevious<CR>")
+
 -- Move lines
 keymap.set("n", "<A-j>", ":m .+1<CR>==")
 keymap.set("n", "<A-k>", ":m .-2<CR>==")
@@ -30,3 +35,10 @@ keymap.set("i", "<A-;>", "<Esc>miA;<Esc>`ii")
 
 -- toggle term start
 keymap.set("n", "<C-\\>", "<CMD>ToggleTerm<CR>")
+
+-- telescope
+
+keymap.set("n", "<leader>ff", "<CMD>lua require('telescope.builtin').find_files()<CR>")
+keymap.set("n", "<leader>fg", "<CMD>lua require('telescope.builtin').live_grep()<CR>")
+keymap.set("n", "<leader>fb", "<CMD>lua require('telescope.builtin').buffers()<CR>")
+keymap.set("n", "<leader>fc", "<CMD>lua require('telescope.builtin').commands()<CR>")
--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 mkdir $HOME/.config/shangar-nvim
-cp -r ./* $HOME/.config/shangar-nvim
+cp -r ./config/* $HOME/.config/shangar-nvim
 echo "alias svim='NVIM_APPNAME=shangar-nvim nvim'" >> $HOME/.zshrc

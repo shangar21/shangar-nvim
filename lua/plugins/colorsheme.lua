a330b874aaaeffbaf1cd18bf9b8bb4ba01e13a46
shangar21 <sharanshangar@gmail.com>
2024-03-09 11:47:55 -0500

    changed colour theme

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -1,16 +1,26 @@
+--return {
+--  {
+--    "navarasu/onedark.nvim",
+--    config = function ()
+--      require("onedark").setup({
+--        theme = "onedark",
+--		style = "darker",
+--        code_style = {
+--          comments = "italic",
+--          functions = "bold"
+--        },
+--      })
+--      vim.cmd("colorscheme " .. "onedark")
+--    end,
+--  }
+--}
+--
 return {
-  {
-    "navarasu/onedark.nvim",
-    config = function ()
-      require("onedark").setup({
-        theme = "onedark",
-		style = "darker",
-        code_style = {
-          comments = "italic",
-          functions = "bold"
-        },
-      })
-      vim.cmd("colorscheme " .. "onedark")
-    end,
-  }
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+		config = function ()
+			vim.opt.termguicolors = true,
+			vim.cmd("colorscheme oxocarbon")
+		end,
+	}
 }
